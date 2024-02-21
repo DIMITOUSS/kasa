93e28e5d23ef03bebae8bc014cad46b9a20705a3
DIMITOUSS <oussama.kechairi@gmail.com>
2024-02-21 21:37:43 +0100

    appartement page up

--- src/components/ApartmentPage/index.jsx
+++ src/components/ApartmentPage/index.jsx
@@ -28,34 +28,50 @@ const ApartmentPage = () => {
     return (
         <div>
             <Header/>
-            <section className='annonce'>
-                <Gallery images={apartment.pictures} />
-                <div className="annonce-info-rating-host-wrapper">
-                    <div className="annonce-info-wrapper">
-                        <h1 className="annonce-title">{apartment.title}</h1>
-                        <p className="annonce-location">{apartment.location}</p>
-                        <div className="tag-wrapper">
-                            {apartment.tags.map((tag, index) => (
-                                <Tag   tagName={tag} key={`${tag}-${index}`} />
-                            ))}
-                        </div>
-                    </div>
-                    <div className='host'>
-
-
-                < div className='host-wrapper'>
-                    <p className='host-name' >{apartment.host.name}</p>
-                    <img className="host-picture" src={apartment.host.picture} alt=""  />
-                </div>
-                </div>
-                </div>
-                <div className="description-equipments-wrapper">
-                    <Collapse title="Description" content={apartment.description} />
-                    <Collapse title="Équipements" content={apartment.equipments} />
-                    <Rating rating={apartment.rating} />
-                </div>
-                
-            </section>
+            <section  className="annonce">
+				<Gallery images={apartment.pictures} />
+				<div className="annonce-info-rating-host-wrapper">
+					<div className="annonce-info-wrapper">
+						<h1 className="annonce-title">{apartment.title}</h1>
+						<p className="annonce-location">
+							{apartment.location}
+						</p>
+						<div className="tag-wrapper">
+							{apartment.tags.map((tag, index) => (
+								<Tag tagName={tag} key={`${tag}-${index}`} />
+							))}
+						</div>
+					</div>
+					<div className="rating-host-wrapper">
+						<Rating rating={apartment.rating} />
+						<div className="host-wrapper">
+                        <div className="host-name">
+  {apartment.host.name.split(' ').map((part, index) => (
+    <React.Fragment key={index}>
+      {part}
+      {index !== apartment.host.name.split(' ').length - 1 && <br />}
+    </React.Fragment>
+  ))}
+</div>
+							<img
+								className="host-picture"
+								src={apartment.host.picture}
+								alt="Host"
+							/>
+						</div>
+					</div>
+				</div>
+				<div className="description-equipments-wrapper">
+					<Collapse
+						title="Description"
+						content={apartment.description}
+					/>
+					<Collapse
+						title="Équipements"
+						content={apartment.equipments}
+					/>
+				</div>
+			</section>
         </div>
     );
 };
