c2a1e5d17556c10a9f9706fbd5d752ec7e4583a3
DIMITOUSS <oussama.kechairi@gmail.com>
2024-02-27 12:23:11 +0100

    update

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Main from  '../../components/Cards/Cards'
+import Header from  '../../components/Header/'
+import Banner from  '../../components/Banner/'
+
+
+
+function index() {
+  return (
+    <div>
+<Header/>
+<Banner/>
+<Main/>
+
+    </div>
+  )
+}
+
+export default index
